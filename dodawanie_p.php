f3bf0b02583b1963ce4fd3ea7076badbf28a724e
jakub-zyrek <jakubzyrek0@gmail.com>
2023-04-03 17:16:28 +0200

    s

--- dodawanie_p.php
+++ dodawanie_p.php
@@ -1,6 +1,6 @@
 <!DOCTYPE html>
 <html lang="pl">
-  <head>
+  <head>abc
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Forum internetowe| Dodawanie pytania</title>
