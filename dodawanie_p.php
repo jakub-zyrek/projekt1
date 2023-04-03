5975fabb75e962980458a2a98bf59101b4c94945
jakub-zyrek <jakubzyrek0@gmail.com>
2023-04-03 17:18:14 +0200

    Zmiana menu

--- dodawanie_p.php
+++ dodawanie_p.php
@@ -1,6 +1,6 @@
 <!DOCTYPE html>
 <html lang="pl">
-  <head>hg
+  <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Forum internetowe| Dodawanie pytania</title>
